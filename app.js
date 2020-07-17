7f3b35355208c7aba9cb7eef5f5a682387bec569
Carlos Freitas <carlos@carlos.freitas.(none)>
2020-07-16 23:17:43 -0300

    Added fetchProduct method.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    message: "Hello World",
+    products: []
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.products = r;
+        });
+    }
+  },
+  created() {
+    this.fetchProducts();
   }
 });
