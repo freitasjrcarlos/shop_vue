aaf5fa0e3570039f528bcd25ae538f7d4753a352
Carlos Freitas <carlos@carlos.freitas.(none)>
2020-07-18 14:37:15 -0300

    Added add and remove item methods.

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el: "#app",
   data: {
     products: [],
-    product: false
+    product: false,
+    car: [],
   },
   filters: {
     priceNumber(value) {
       return value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    }
+  },
+  computed: {
+    totalCar() {
+      let total = 0;
+      if (this.car.length) {
+        this.car.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -35,9 +47,28 @@ const vm = new Vue({
       if (target == currentTarget) {
         this.product = false;
       }
+    },
+    addItem() {
+      this.product.estoque--;
+      const { id, nome, preco } = this.product;
+      this.car.push({ id, nome, preco });
+    },
+    removeItem(index) {
+      this.car.splice(index, 1);
+    },
+    checkLocalStorage() {
+      if (window.localStorage.car) {
+        this.car = JSON.parse(window.localStorage.car);
+      }
+    },
+  },
+  watch: {
+    car() {
+      window.localStorage.car = JSON.stringify(this.car);
     }
   },
   created() {
     this.fetchProducts();
+    this.checkLocalStorage();
   }
 });
